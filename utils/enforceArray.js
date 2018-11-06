a4d0f9747a48533dd14cdade5938a6cb7e188f9c
Caleb McQuillin <codebycaleb@users.noreply.github.com>
2018-11-05 21:39:02 -0500

    Handle `undefined` results
    When retrieving issues, the returned structure may not contain the
    expected items (i.e. it normally returns <XS><X></X><X></X></XS>, but
    may return <XS></XS>). This commit replaces undefined results with [].

--- utils/enforceArray.js
+++ utils/enforceArray.js
@@ -1,6 +1,8 @@
 module.exports = (response, z, bundle) => {
   if (Array.isArray(response)) {
     return response;
+  } else if (response == undefined) {
+    return [];
   }
   return [response];
 }
